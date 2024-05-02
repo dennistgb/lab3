3947f82d5abe2f014230049d8bcf3681624d07ec
Dennis <dennistgb579@gmail.com>
2024-05-02 12:25:55 +0800

    updated from lab2

---
+++ Test_Lab2.py
@@ -0,0 +1,22 @@
+import lab2.lab2 as lab2
+
+def test_min_max():
+    result = []
+    input_arr = ["1","100","111"]
+    ans = ['max = 111', 'min = 1']
+    result = lab2.calc_min_max_temperature(input_arr)
+
+    assert (result == ans)
+
+def test_avg():
+    result = []
+    input_arr = ["3","4","5"]
+    result = lab2.calc_average_temperature(input_arr)
+
+    assert (result == 4.0)
+
+def test_median():
+    input_arr = ["1","100","111"]
+    result = lab2.median_temp(input_arr)
+
+    assert (result == 100)
