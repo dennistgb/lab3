b704e0950f12ca27ef86c3ffd48ec52ebfeb77c9
Dennis <dennistgb579@gmail.com>
2024-05-02 10:49:01 +0800

    added .gitignore

---
+++ Test_bmi.py
@@ -0,0 +1,13 @@
+import lab2.bmi as bmi
+
+def test_bmi_normal_weight():
+    result = bmi.calculate_bmi(1.73,57)
+    assert (result == 0)
+
+def test_bmi_over_weight():
+    result = bmi.calculate_bmi(1.73,150)
+    assert (result == 1)
+
+def test_bmi_under_weight():
+    result = bmi.calculate_bmi(1.73,400)
+    assert (result == -1)
